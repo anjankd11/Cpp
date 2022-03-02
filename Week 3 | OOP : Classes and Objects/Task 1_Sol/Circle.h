32341f693170e20801207673bd81a4d01dd31d95
Anjan Kumar Das <anjaniscoding@gmail.com>
2022-03-03 04:22:22 +0600

    Create Circle.h

---
+++ Week 3 | OOP : Classes and Objects/Task 1_Sol/Circle.h
@@ -0,0 +1,14 @@
+//declaration of class , it's attributes and methods constructors
+
+class circle {
+
+private:                            //should not be accessible from outside the class
+	double R;          
+
+public:
+	circle();                       // 2 types of constructors for different input parameters which are defined in Circle.cpp
+	circle(double pR); 
+
+	void print();                   //function that prints all information about the rectangle: its sides, area and perimeter values
+
+};
